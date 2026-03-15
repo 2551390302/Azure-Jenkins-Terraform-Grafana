4ca2cbf11f3f90c5e6cfcf4443a66da4a42a6bf8
李勇 <2551390302@qq.com>
2026-03-15 13:27:52 +0800

    init

--- Terraform/environments/dev/main.tf
+++ Terraform/environments/dev/main.tf
@@ -9,7 +9,7 @@ module "networking" {
   source = "../../modules/networking"
 
   resource_group_name = "rg-kk01-eas-second-kerwin02" # VNet 所在的资源组
-  existing_vnet_name  = "vnet-kk02-eas-devops02"      # 现有 VNet 名称
+  existing_vnet_name  = "vnet-kk02-eas-devops01"      # 现有 VNet 名称
   aks_subnet_prefix   = ["10.0.1.0/24"]               # 新子网的地址范围
   create_vnet         = false                         # 关键：不创建新 VNet
 
