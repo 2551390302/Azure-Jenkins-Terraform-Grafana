cd69cad71f677adbad17d00697631ef1f36b2bd9
李勇 <2551390302@qq.com>
2026-03-15 21:38:56 +0800

    init

--- Terraform/environments/dev/main.tf
+++ Terraform/environments/dev/main.tf
@@ -55,7 +55,7 @@ module "aks" {
   kubernetes_version  = "1.33.7"
 
   # 使用最便宜的 VM 大小（B 系列，开发/测试适用）
-  vm_size         = "Standard_DS2_v2" # 2 vCPU, 4 GB 内存，可运行少量容器
+  vm_size         = "Standard_D2s_v5"
   os_disk_size_gb = 128
 
   # 子网 ID 必须是完整的 Azure 资源 ID，而不是仅 VNet 名称！
