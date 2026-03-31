862fe7b7650a59b29b5211e1dfb27b61790dc2ab
李勇 <2551390302@qq.com>
2026-03-31 23:54:46 +0800

    change repository of helm

--- Terraform/environments/dev/main.tf
+++ Terraform/environments/dev/main.tf
@@ -117,7 +117,7 @@ resource "kubernetes_namespace" "monitoring" {
 # 使用 Helm 部署 kube-prometheus-stack
 resource "helm_release" "prometheus_stack" {
   name       = "prometheus"
-  repository = "https://prometheus-community.github.io/helm-charts"
+  repository = "http://mirror.azure.cn/kubernetes/helm"
   chart      = "kube-prometheus-stack"
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
 
